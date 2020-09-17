7daf8f6036405d3face047ef2cc787259ef9fefa
Andy Cole <tranthenhut@gmail.com>
2020-09-17 07:12:25 +0700

    Update SortCharactersByFrequency.java

--- SortCharactersByFrequency.java
+++ SortCharactersByFrequency.java
@@ -34,3 +34,89 @@ Explanation:
 Note that 'A' and 'a' are treated as two different characters.
 -----------------------------
 
+class Solution {
+    
+    public String frequencySort(String s) {
+        
+        
+        return solution2(s);
+        
+    }
+    
+    public String solution2(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        //convert string to map with key = character, value = number of character which appeared in String
+        for (int i = 0; i < s.length(); ++i){            
+            Character c = Character.valueOf(s.charAt(i));
+            
+            if (map.containsKey(c)){
+                int value = map.get(c);
+                map.replace(c, value+1);                    
+            }
+            else {
+                map.put(c, 1);
+            }            
+        }
+        
+        //convert the map to String array, with index is the frequency of character
+        StringBuilder[] arr = new StringBuilder[s.length() + 1];
+        
+        for (Map.Entry<Character, Integer> entry : map.entrySet()){
+            Character c = entry.getKey();
+            Integer frequency = entry.getValue();
+            
+            if (arr[frequency] == null){
+                arr[frequency] = new StringBuilder(c.toString());
+            }
+            else {
+                arr[frequency].append(c.toString());
+            }            
+        }
+        
+        //loop arr to from hight to low
+        StringBuilder builder = new StringBuilder();
+        for (int i = arr.length-1; i >= 0; --i){            
+            if (arr[i] == null) continue;            
+            String value = arr[i].toString();
+            
+            for (int j = 0; j < value.length(); ++j){
+                char c = value.charAt(j);                
+                for (int k = 0; k < i; ++k){
+                    builder.append(c);
+                }
+
+            }
+        }
+        
+        return builder.toString();        
+    }
+    
+    
+    public String solution1(String s) {
+        if(s.length()==0)return ""; 
+      
+        HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+      
+        List<Character> count=new ArrayList<>(map.keySet());
+        Collections.sort(count,(a, b) -> map.get(b) - map.get(a));
+      
+        StringBuilder sb=new StringBuilder();
+        for(char c:count){
+            int n=map.get(c);
+            for(int i=0;i<n;i++){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+        
+    }
+    
+}
+  
+  
+
